b669184001bf3e1b2f2c71d13c8babb64b2bfc57
Trent <trentthebaker@gmail.com>
2021-12-01 11:24:59 -0400

    Finish day 1 part one

--- day_1/main.py
+++ day_1/main.py
@@ -1,2 +1,19 @@
-f = open("input.txt", "r")
-print(f.read())
+lines = open('day_1/input.txt', 'r').read().splitlines()
+
+increase_count = 0
+decrease_count = 0
+
+num_lines = [int(line) for line in lines]
+prev = -1
+for num in num_lines:
+    if prev != -1:
+        if prev < num:
+            increase_count += 1
+        elif prev > num:
+            decrease_count += 1
+    prev = num
+
+
+print(f'Total lines: {len(num_lines)}')
+print(f'Total increases: {increase_count}')
+print(f'Total decreases: {decrease_count}')
