318c362e517a38505969417a77c5f31d306fce66
Trent Baker <TrentBaker@dal.ca>
2021-12-02 10:47:25 -0400

    Done day 2 part 1

---
+++ day_2/main.py
@@ -0,0 +1,23 @@
+lines = open('day_2/input.txt', 'r').read().splitlines()
+
+commands = []
+for line in lines:
+    line = line.split()
+    commands.append((line[0], int(line[1])))
+    
+    
+h_pos = 0
+depth = 0
+
+for command in commands:
+    if command[0] == 'forward':
+        h_pos += command[1]
+    elif command[0] == 'down':
+        depth += command[1]
+    elif command[0] == 'up':
+        depth -= command[1]
+        
+
+print(f'Horizontal position: {h_pos}')
+print(f'Depth: {depth}')
+print(f'Product: {h_pos * depth}')
