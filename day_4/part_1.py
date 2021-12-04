58e38df207ba4ea7f2ea85c13a4f0b04f73e23f0
Trent Baker <TrentBaker@dal.ca>
2021-12-04 13:00:03 -0400

    Finish day 4 part 2

--- day_4/part_1.py
+++ day_4/part_1.py
@@ -1,24 +1,21 @@
-lines = open('day_4/input.txt', 'r').read().splitlines()
 
-# List of numbers called
-called = [int(num) for num in lines[0].split(',')]
+def make_boards(boards_raw):
+    boards = []
 
-# Create boards as 2D lists
-boards_raw = '\n'.join(lines[2:]).split('\n\n')
-boards = []
-
-for board_raw in boards_raw:
-    rows = board_raw.split('\n')
-    board = []
-    for row in rows:
-        new_row = []
-        cells = row.split()
-        for cell in cells:
-            new_row.append(int(cell))
-        if len(new_row) > 0:
-            board.append(new_row)
-    if len(board) > 0:
-        boards.append(board)
+    for board_raw in boards_raw:
+        rows = board_raw.split('\n')
+        board = []
+        for row in rows:
+            new_row = []
+            cells = row.split()
+            for cell in cells:
+                new_row.append(int(cell))
+            if len(new_row) > 0:
+                board.append(new_row)
+        if len(board) > 0:
+            boards.append(board)
+            
+    return boards
     
 def check_win(board):
     # Check rows
@@ -64,17 +61,54 @@ def find_winning_board_and_number(called, boards):
                         if check_win(board):
                             return (called_num, board)
 
-if __name__ == '__main__':
+
+def part_1(called: list, boards: list):
     winning_num, winning_board = find_winning_board_and_number(called, boards)
-    
+
     print(f'Winning number: {winning_num}')
     print(f'Winning board: {winning_board}')
-    
+
     board_unmarked_sum = 0
     for row in winning_board:
         for cell in row:
             if cell > 0:
                 board_unmarked_sum += cell
-    
+
     print(f'Sum of unmarked numbers: {board_unmarked_sum}')
     print(f'Product of winning number and sum: {winning_num * board_unmarked_sum}')
+
+
+def part_2(called:list, boards:list):
+    while len(boards) > 1:
+        winning_num, winning_board = find_winning_board_and_number(called, boards)
+        boards.remove(winning_board)
+
+    winning_num, winning_board = find_winning_board_and_number(called, boards)
+    
+    print(f'Last winning number: {winning_num}')
+    print(f'Last winning board: {winning_board}')
+
+    board_unmarked_sum = 0
+    for row in winning_board:
+        for cell in row:
+            if cell > 0:
+                board_unmarked_sum += cell
+
+    print(f'Sum of unmarked numbers: {board_unmarked_sum}')
+    print(f'Product of winning number and sum: {winning_num * board_unmarked_sum}')
+
+
+
+
+if __name__ == '__main__':
+    lines = open('day_4/input.txt', 'r').read().splitlines()
+
+    # List of numbers called
+    called = [int(num) for num in lines[0].split(',')]
+
+    # Create boards as 2D lists
+    boards_raw = '\n'.join(lines[2:]).split('\n\n')
+    boards = make_boards(boards_raw)
+    
+    part_1(called, boards)
+    part_2(called, boards)
